13da93d4c6bef975131c3139980fea9afd883cdf
jinsol kim <jinsol032360@gmail.com>
2025-07-28 14:27:02 +0900

    merge: dev 브랜치 병합 및 컨트롤러/라우터 충돌 해결

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import { errorHandler } from './middlewares/error-moddleware';
-
-<<<<<<< HEAD
 import { errorHandler } from './utils/error';
 import routes from './routes/index-route';
-
-dotenv.config();
-=======
-import routes from './routes/index-route';
->>>>>>> 1daebffdab2725e2bdaacdba2d41220d2b656c9f
 
 const app = express();
 
@@ -25,14 +17,6 @@ app.use(express.urlencoded({ extended: true }));
 
 // 라우터 등록
 app.use('/', routes);
-<<<<<<< HEAD
-
-// 에러 미들웨어
-app.use(errorHandler);
-
-const port = process.env.PORT;
-=======
->>>>>>> 1daebffdab2725e2bdaacdba2d41220d2b656c9f
 
 // 에러 미들웨어 등록
 app.use(errorHandler);
