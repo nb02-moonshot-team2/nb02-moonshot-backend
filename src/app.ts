fb4b54413948c5ba25db68ad9b023f606cecf3a6
jinsol kim <jinsol032360@gmail.com>
2025-07-28 10:19:12 +0900

    chore: 에러 핸들러 미들웨어 app에 등록

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import { errorHandler } from './utils/error';
 import routes from './routes/index-route';
 
 dotenv.config();
@@ -11,6 +12,9 @@ app.use(express.json());
 
 // 라우터 등록
 app.use('/', routes);
+
+// 에러 미들웨어
+app.use(errorHandler);
 
 const port = process.env.PORT;
 
