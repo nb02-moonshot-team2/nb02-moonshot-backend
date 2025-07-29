6a0cc4d314248663b325032d4d590d28f2068ddf
jinsol kim <jinsol032360@gmail.com>
2025-07-29 22:23:13 +0900

    refactor: 멤버 관련 DTO 수정(Express Request 확장 타입 정의)

--- src/utils/dtos/member-dto.ts
+++ src/utils/dtos/member-dto.ts
@@ -1,3 +1,5 @@
+import { Request } from 'express';
+
 export interface ProjectMemberResponse {
   id: number;
   name: string;
@@ -8,6 +10,19 @@ export interface ProjectMemberResponse {
   invitationId: number;
 }
 
+export interface AuthenticateRequest extends Request {
+  user?: {
+    id: number;
+    email: string;
+    nickname: string;
+    password: string;
+    image: string | null;
+    refreshToken: string | null;
+    createdAt: Date;
+    updatedAt: Date;
+  };
+}
+
 export interface GetProjectMembersQuery {
   page: number;
   limit: number;
@@ -15,4 +30,9 @@ export interface GetProjectMembersQuery {
 
 export interface InviteMember {
   email: string;
+}
+
+export interface AcceptInvitationParams {
+  invitationId: number;
+  userId: number;
 }
