84fa9bd75909cda26f22b3352ecbd4505dd8c1eb
고민재 <min>
2025-07-31 17:16:04 +0900

    fix: 하위 할 일 라우트변경, passport api test

--- src/routes/index-route.ts
+++ src/routes/index-route.ts
@@ -3,11 +3,14 @@ import memberRouter from './member-route';
 import authRouter from './auth-route';
 import userRouter from './user-route';
 import taskRouter from './task-route';
+import subtaskRouter from './subtask-route';
+
 const router = Router();
 
 router.use('/', memberRouter);
 router.use('/auth', authRouter);
 router.use('/users', userRouter);
 router.use('/', taskRouter);
+router.use('/subtasks', subtaskRouter);
 
 export default router;
