e0310065f9c89fbf6464919bcf965f2e4d8423fd
Kang si yeon <rkdtldus0115@naver.com>
2025-08-05 10:13:30 +0900

    fix: 주석 제거

--- src/utils/dtos/project-dto.ts
+++ src/utils/dtos/project-dto.ts
@@ -1,11 +1,9 @@
-// 프로젝트 생성 시 필요한 데이터
 export interface CreateProjectDTO {
   creatorId: number;
   name: string;
   description: string;
 }
 
-// 프로젝트 수정 시 필요한 데이터
 export interface UpdateProjectDTO {
   creatorId: number;
   projectId: number;
@@ -13,7 +11,6 @@ export interface UpdateProjectDTO {
   description?: string;
 }
 
-// 프로젝트 삭제 시 필요한 데이터
 export interface DeleteProjectDTO {
   creatorId: number;
   projectId: number;
