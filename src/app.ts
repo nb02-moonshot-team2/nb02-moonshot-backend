35aa59857ca9b0a45ab92ee3e4294ebf1a870692
chya-chya <cktndus32@naver.com>
2025-08-13 13:19:40 +0900

    fix:구글 로그인 수정

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const app = express();
 // 미들웨어 설정
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://nb-02-moon-shot-fe.vercel.app'],
     credentials: true,
   })
 );
